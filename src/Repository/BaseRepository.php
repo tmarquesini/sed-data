1e4b3fb083af32bcfc51dea2c5794a551b828e33
Thiago Marquesini <contato@marquesini.com.br>
2018-05-10 15:43:05 -0300

    Cria o repositório base

--- src/Repository/BaseRepository.php
+++ src/Repository/BaseRepository.php
@@ -9,7 +9,25 @@
 namespace SedData\Repository;
 
 
+use SedData\Http;
+
+/**
+ * Class BaseRepository
+ * @package SedData\Repository
+ */
 class BaseRepository
 {
+    /**
+     * @var Http
+     */
+    protected $http;
 
+    /**
+     * BaseRepository constructor.
+     * @param Http $http
+     */
+    public function __construct(Http $http)
+    {
+        $this->http = $http;
+    }
 }
