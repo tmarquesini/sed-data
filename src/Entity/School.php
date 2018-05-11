8073765b506de934a31278761db6add85263feb9
Thiago Marquesini <contato@marquesini.com.br>
2018-05-11 08:58:48 -0300

    Classe e repositório para escolas

--- src/Entity/School.php
+++ src/Entity/School.php
@@ -1,15 +1,48 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: thiago
- * Date: 10/05/18
- * Time: 15:21
- */
 
 namespace SedData\Entity;
 
 
+/**
+ * Class School
+ * @package SedData\Entity
+ */
 class School
 {
+    /**
+     * @var string
+     */
+    private $code;
 
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * School constructor.
+     * @param string $code
+     * @param string $name
+     */
+    public function __construct(string $code, string $name)
+    {
+        $this->code = $code;
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 }
--- src/Repository/SchoolsRepository.php
+++ src/Repository/SchoolsRepository.php
@@ -1,15 +1,64 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: thiago
- * Date: 10/05/18
- * Time: 15:21
- */
 
 namespace SedData\Repository;
 
+use SedData\Entity\School;
+use Doctrine\Common\Collections\ArrayCollection;
 
-class SchoolsRepository
+/**
+ * Class SchoolsRepository
+ * @package SedData\Repository
+ */
+class SchoolsRepository extends BaseRepository
 {
+    /**
+     * @return ArrayCollection
+     */
+    public function getAll()
+    {
+        $data = $this->getData();
 
+        $schools = new ArrayCollection();
+
+        foreach($data as $school) {
+            $code = $school->Value;
+            $name = $this->extractSchoolName($school->Text);
+
+            $schools->add(
+                new School($code, $name)
+            );
+        }
+
+        return $schools;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getData()
+    {
+        $data = [
+            'diretoria' => 24536,
+            'municipio' => 9501,
+            'redeEnsino' => null,
+            'situacaoFuncionamentoEscola' => 1
+        ];
+
+        $url = 'nca/RelacaoAlunosClasse/DropDownEscolasCIEJson' . '?' . http_build_query($data);
+
+        $response = $this->http->get($url);
+
+        return json_decode((string) $response->getBody());
+    }
+
+    /**
+     * @param string $text
+     * @return bool|string
+     */
+    protected function extractSchoolName(string $text)
+    {
+        $nameStartPosition = strpos($text, ' - ') + 3;
+
+        return substr($text, $nameStartPosition);
+    }
 }
